0d8e328300e2f3e7f5920e7f8a1916d537021449
Dmitry Renev <52632809+dmtrnv@users.noreply.github.com>
2022-06-13 20:09:47 +0500

    Delete лр7 directory

--- лр7/lab7.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW lab2query1 AS SELECT FullName, PhoneNumber, Salary FROM Employees;
-CREATE VIEW lab2query2 AS SELECT FullName, Town FROM Employees ORDER BY Town;
-CREATE VIEW lab2query3 AS SELECT FullName, WorkExperience FROM Employees WHERE WorkExperience > 4;
-
-SELECT * FROM Employees;
-SELECT * FROM lab2query1;
-SELECT * FROM lab2query2;
-SELECT * FROM lab2query3;
-SELECT FullName FROM lab2query3;
